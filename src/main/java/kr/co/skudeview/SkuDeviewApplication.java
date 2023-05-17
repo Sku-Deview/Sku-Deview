ea085c0c81639f2c6a816ceab7fe090e6d992ae6
soohykeee <gur1028@skuniv.ac.kr>
2023-05-17 20:40:08 +0900

    Init Commit

---
+++ src/main/java/kr/co/skudeview/SkuDeviewApplication.java
@@ -0,0 +1,13 @@
+package kr.co.skudeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuDeviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkuDeviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/skudeview/SkuDeviewApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.skudeview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuDeviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
