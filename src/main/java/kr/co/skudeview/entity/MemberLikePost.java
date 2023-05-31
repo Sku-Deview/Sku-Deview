d8c21a0b6a0f3b9009c19a1f1664e07ce14d36b9
soohykeee <gur1028@skuniv.ac.kr>
2023-05-31 23:42:55 +0900

    feat : member와 post, reply 사이의 다대다 관계인 like를 연관테이블로 생성

---
+++ src/main/java/kr/co/skudeview/entity/MemberLikePost.java
@@ -0,0 +1,34 @@
+package kr.co.skudeview.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberLikePost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberLikePost_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public MemberLikePost(Member member,
+                          Post post) {
+        this.member = member;
+        this.post = post;
+    }
+
+}
---
+++ src/main/java/kr/co/skudeview/entity/MemberLikeReply.java
@@ -0,0 +1,34 @@
+package kr.co.skudeview.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberLikeReply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberLikeReply_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id")
+    private Reply reply;
+
+    @Builder
+    public MemberLikeReply(Member member,
+                           Reply reply) {
+        this.member = member;
+        this.reply = reply;
+    }
+
+}
