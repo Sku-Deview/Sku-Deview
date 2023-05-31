bcc14263c1b5b0d9693680900875368fe9a270fb
soohykeee <gur1028@skuniv.ac.kr>
2023-05-31 23:42:14 +0900

    feat : Post, Image, Video, Reply, Report entity 생성

---
+++ src/main/java/kr/co/skudeview/entity/Reply.java
@@ -0,0 +1,45 @@
+package kr.co.skudeview.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(name = "content")
+    @NotNull
+    private String content;
+
+    @Column(name = "like_count", columnDefinition = "INTEGER DEFAULT 0")
+    private int likeCount;
+
+    @Builder
+    public Reply(Member member,
+                 Post post,
+                 String content,
+                 int likeCount) {
+        this.member = member;
+        this.post = post;
+        this.content = content;
+        this.likeCount = likeCount;
+    }
+}
