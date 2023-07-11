cc5569d0c70a16d49575b8619dc9b742a2e02f1a
soohykeee <gur1028@skuniv.ac.kr>
2023-07-11 17:28:14 +0900

    fix : Jpa의 Query Creation을 통해 삭제되지 않은 데이터를 DB에서 가져올 수 있도록 수정

--- src/main/java/kr/co/skudeview/repository/ReplyRepository.java
+++ src/main/java/kr/co/skudeview/repository/ReplyRepository.java
@@ -2,10 +2,24 @@ package kr.co.skudeview.repository;
 
 import kr.co.skudeview.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface ReplyRepository extends JpaRepository<Reply,Long> {
 
-    List<Reply> findAllByPostId(Long postId);
+    // replyId 값으로 reply 단일 조회 + 삭제되지 않은 reply
+    Optional<Reply> findReplyByIdAndDeleteAtFalse(Long id);
+
+    // reply 다중 조회 (작성자, 게시글 상관없이) + 삭제되지 않은 reply
+    List<Reply> findAllByDeleteAtFalse();
+
+    // postId 값으로 reply 다중 조회 (게시글) + 삭제되지 않은 reply
+    List<Reply> findRepliesByPostIdAndDeleteAtFalse(Long postId);
+
+    // memberEmail 값으로 reply 다중 조회 (작성자) + 삭제되지 않은 reply
+    List<Reply> findRepliesByMemberEmailAndDeleteAtFalse(String memberEmail);
+
 }
