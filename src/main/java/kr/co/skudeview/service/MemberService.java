ca47908a2076463c9e9e1c1f0d36daf7421c9388
soohykeee <gur1028@skuniv.ac.kr>
2023-07-17 16:28:54 +0900

    refactor : update 및 delete 시 발생하던 로직 오류 수정 + reponse 시 memberSkill 의 중복 출력을 방지하기 위한 Set 사용

--- src/main/java/kr/co/skudeview/service/MemberService.java
+++ src/main/java/kr/co/skudeview/service/MemberService.java
@@ -1,9 +1,14 @@
 package kr.co.skudeview.service;
 
+import kr.co.skudeview.domain.Member;
+import kr.co.skudeview.domain.enums.Gender;
+import kr.co.skudeview.domain.enums.Role;
 import kr.co.skudeview.service.dto.request.MemberRequestDto;
 import kr.co.skudeview.service.dto.response.MemberResponseDto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public interface MemberService {
 
@@ -16,6 +21,38 @@ public interface MemberService {
     void updateMember(MemberRequestDto.UPDATE update);
 
     void deleteMember(Long id);
+
+    Set<String> getSkillsNameByMember(Member member);
+
+    default Member toEntity(MemberRequestDto.CREATE create) {
+        return Member.builder()
+                .email(create.getEmail())
+                .password(create.getPassword())
+                .name(create.getName())
+                .nickname(create.getNickname())
+                .telephone(create.getTelephone())
+                .address(create.getAddress())
+                .birthDate(create.getBirthDate())
+                .gender(Gender.valueOf(create.getGender()))
+                .role(Role.valueOf(create.getRole()))
+                .memberSkills(Collections.emptyList())
+                .build();
+    }
+
+    default MemberResponseDto.READ toReadDto(Member member) {
+        return MemberResponseDto.READ.builder()
+                .memberId(member.getId())
+                .email(member.getEmail())
+                .name(member.getName())
+                .nickname(member.getNickname())
+                .telephone(member.getTelephone())
+                .address(member.getAddress())
+                .birthDate(member.getBirthDate())
+                .gender(String.valueOf(member.getGender()))
+                .role(String.valueOf(member.getGender()))
+                .skillName(getSkillsNameByMember(member))
+                .build();
+    }
 
 }
 
