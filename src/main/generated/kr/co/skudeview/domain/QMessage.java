1acb94107acee0a5cec6ec213fb8dd52ba74c3fa
jinhoLee <kswlsgh@naver.com>
2023-07-25 14:05:22 +0900

    기존에 설정해둔 build.gradle 은 springboot 2.x.x 버전으로 현재 프로젝트에 설정된springboot 3.x.x 버전으로 변경

---
+++ src/main/generated/kr/co/skudeview/domain/QMessage.java
@@ -0,0 +1,67 @@
+package kr.co.skudeview.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMessage is a Querydsl query type for Message
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMessage extends EntityPathBase<Message> {
+
+    private static final long serialVersionUID = 1899250499L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMessage message = new QMessage("message");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final BooleanPath deleteAt = _super.deleteAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modDate = _super.modDate;
+
+    public final QMember receiver;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    public final QMember sender;
+
+    public QMessage(String variable) {
+        this(Message.class, forVariable(variable), INITS);
+    }
+
+    public QMessage(Path<? extends Message> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMessage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMessage(PathMetadata metadata, PathInits inits) {
+        this(Message.class, metadata, inits);
+    }
+
+    public QMessage(Class<? extends Message> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.receiver = inits.isInitialized("receiver") ? new QMember(forProperty("receiver")) : null;
+        this.sender = inits.isInitialized("sender") ? new QMember(forProperty("sender")) : null;
+    }
+
+}
+
