eaf0420eeb9bb24caf3ab7deb839f5b634606ba5
jinhoLee <kswlsgh@naver.com>
2023-09-05 22:06:59 +0900

    refactor : 게시판 제목 subString 20글자로 수정

--- frontend/src/views/board/BoardList.vue
+++ frontend/src/views/board/BoardList.vue
@@ -17,8 +17,8 @@
       <tr v-for="(item, idx) in list" :key="idx">
         <td>{{ item.postId }}</td>
         <td>{{ item.postCategory }}</td>
-        <td v-if="item.title.length<30"><a v-on:click="fnView(`${item.postId}`)">{{ item.title }}</a></td>
-        <td v-else><a v-on:click="fnView(`${item.postId}`)">{{ item.title.substring(0,30)+". . . ." }}</a></td>
+        <td v-if="item.title.length<20"><a v-on:click="fnView(`${item.postId}`)">{{ item.title }}</a></td>
+        <td v-else><a v-on:click="fnView(`${item.postId}`)">{{ item.title.substring(0,20)+"..." }}</a></td>
         <td>{{ item.memberNickname }}</td>
         <td>{{ item.regDate }}</td>
       </tr>
