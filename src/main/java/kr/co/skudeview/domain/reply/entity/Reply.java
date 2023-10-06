242b0c324c267bedea1eaff11cc850f59b93868f
wlshooo <kswlsgh@naver.com>
2023-10-07 03:15:01 +0900

    fix : content에 varchar(255)이상의 값이 들어오면 Data truncation: Data too long for column 이 발생하여 dataType을 longText로 변경

--- src/main/java/kr/co/skudeview/domain/reply/entity/Reply.java
+++ src/main/java/kr/co/skudeview/domain/reply/entity/Reply.java
@@ -28,6 +28,7 @@ public class Reply extends BaseEntity {
     @JoinColumn(name = "post_id")
     private Post post;
 
+    //초기에 varchar(255)로 설정하였으나 내용을 초과하는 내용이 들어올 시 버그가 발생하여 dataType을 longText로 변경
     @Column(name = "content")
     @NotNull
     private String content;
